7b326180edf1cd3eea4a3fd657d0a7fda262db54
1337023373 <1337023373@qq.com>
2023-12-22 23:45:21 +0800

    feat():完善了一对多的映射的sql语句

--- spzx-service/service-order/src/main/java/com/atguigu/spzx/order/mapper/OrderInfoMapper.java
+++ spzx-service/service-order/src/main/java/com/atguigu/spzx/order/mapper/OrderInfoMapper.java
@@ -2,6 +2,7 @@ package com.atguigu.spzx.order.mapper;
 
 import com.atguigu.spzx.model.dto.h5.OrderInfoDto;
 import com.atguigu.spzx.model.entity.order.OrderInfo;
+import feign.Param;
 
 import java.util.List;
 
@@ -10,5 +11,5 @@ public interface OrderInfoMapper {
 
     OrderInfo getOrderInfo(Long orderId);
 
-    List<OrderInfo> orderInfo(Long userInfoId, Integer orderStatus);
+    List<OrderInfo> orderInfo(@Param("userInfoId") Long userInfoId, @Param("orderStatus") Integer orderStatus);
 }
